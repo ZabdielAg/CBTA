412f3e93e70490b1df66a3c0bcff1350aa2786a4
Zabdiel Adalid González <129902623+ZabdielAg@users.noreply.github.com>
2025-02-17 14:34:25 -0600

    Add files via upload

---
+++ Script/script.js
@@ -0,0 +1,46 @@
+document.querySelectorAll('.grid-img-container').forEach(container => {
+    container.addEventListener('click', function() {
+        const clickText = this.querySelector('.grid-click-text');
+        if (clickText.style.display === "none" || clickText.style.display === "") {
+            clickText.style.display = "block";
+        } else {
+            clickText.style.display = "none";
+        }
+    });
+});
+
+ // JavaScript for opening the search panel
+ document.getElementById('searchIcon').addEventListener('click', function() {
+    const searchPanel = document.getElementById('searchPanel');
+    if (searchPanel.style.display === 'none' || searchPanel.style.display === '') {
+        searchPanel.style.display = 'block';
+    } else {
+        searchPanel.style.display = 'none';
+    }
+});
+
+// Close search panel when clicking outside
+document.addEventListener('click', function(event) {
+    const searchIcon = document.getElementById('searchIcon');
+    const searchPanel = document.getElementById('searchPanel');
+    if (!searchPanel.contains(event.target) && !searchIcon.contains(event.target)) {
+        searchPanel.style.display = 'none';
+    }
+});
+
+let lastScrollTop = 0;
+window.addEventListener("scroll", function () {
+    const navbarScroll = document.getElementById("navbar-scroll");
+    let currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    
+    if (currentScrollTop > lastScrollTop) {
+        // Scroll hacia abajo
+        navbarScroll.style.display = "block";
+    } else {
+        // Scroll hacia arriba
+        if (currentScrollTop <= 100) { // Ajusta este valor si es necesario
+            navbarScroll.style.display = "none";
+        }
+    }
+    lastScrollTop = currentScrollTop;
+});
